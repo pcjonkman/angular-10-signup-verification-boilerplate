f532b5825a552b1715a2f7cb3ac0425723ce148a
Peter Jonkman <github@pcjonkman.nl>
2022-11-01 22:42:42 +0100

    resolve different way

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -5,6 +5,6 @@ export function appInitializer(accountService: AccountService) {
         // attempt to refresh token on app start up to auto authenticate
         accountService.refreshToken()
             .subscribe()
-            .add(() => resolve(''));
+            .add(resolve(null));
     });
 }
